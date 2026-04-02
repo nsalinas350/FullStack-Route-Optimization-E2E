fa8ac7f663ec307c89fc05d8147d38bc9f7a51ba
nsalinas350 <nsalinas@unal.edu.co>
2026-04-02 18:59:59 -0500

    feat: Backend connection established

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,26 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.staticfiles import StaticFiles 
+from fastapi.responses import RedirectResponse
 import uvicorn
 import sys
 import os
+import pandas as pd
 
 # In order to access to the folders in the project
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from optimization_engine.optimize_routes import solve_logistics_route
 from optimization_engine.visualize_route import generate_route_map
 
-import pandas as pd
 
 app = FastAPI(title="Logismart API")
+
+# Create the output folder, if not exitst
+if not os.path.exists("outputs"):
+    os.makedirs("outputs")
+
+# Serve the path file as a static resource
+app.mount("/maps", StaticFiles(directory="outputs"), name="maps")
+
 
 @app.get("/run-optimization")
 def run_tsp(precision: float = 0.15, seconds: int = 600):
@@ -19,7 +28,10 @@ def run_tsp(precision: float = 0.15, seconds: int = 600):
     base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     csv_file = os.path.join(base_path, 'data', 'medellin_customers.csv')
     matrix_file = os.path.join(base_path, 'data', 'distance_matrix.npy')
-    output_map = os.path.join(base_path, 'route_map.html')
+    # output_map = os.path.join(base_path, 'route_map.html')
+
+    output_map_name = "route_medellin.html"
+    output_path = os.path.join("outputs", output_map_name)
 
     try:
         # Ejecutar el motor
@@ -33,7 +45,7 @@ def run_tsp(precision: float = 0.15, seconds: int = 600):
         if sequence:
             # Generar mapa
             df = pd.read_csv(csv_file)
-            generate_route_map(df, sequence, output_file=output_map)
+            generate_route_map(df, sequence, output_file=output_path)
             
             return {
                 "message": "Optimization Complete",
@@ -42,8 +54,12 @@ def run_tsp(precision: float = 0.15, seconds: int = 600):
                     "gap_target": precision,
                     "max_time_sec": seconds
                 },
+                "map_url": f"http://127.0.0.1:8000/maps/{output_map_name}",
                 "path": sequence
             }
+        
+            # If you only want the map
+            return RedirectResponse(url=f"/maps/{output_map_name}")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
