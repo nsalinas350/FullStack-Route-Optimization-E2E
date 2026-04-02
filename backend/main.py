7eed1bf2ee43db926bbca178522a992dc856dba2
nsalinas350 <nsalinas@unal.edu.co>
2026-04-02 16:13:36 -0500

    Update main.py

---
+++ backend/main.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI, HTTPException
+import uvicorn
+import sys
+import os
+
+# In order to access to the folders in the project
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from optimization_engine.optimize_routes import solve_logistics_route
+from optimization_engine.visualize_route import generate_route_map
+
+import pandas as pd
+
+app = FastAPI(title="Logismart API")
+
+@app.get("/run-optimization")
+def run_tsp(precision: float = 0.15, seconds: int = 600):
+    # Definimos las rutas desde la raíz del proyecto
+    base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    csv_file = os.path.join(base_path, 'data', 'medellin_customers.csv')
+    matrix_file = os.path.join(base_path, 'data', 'distance_matrix.npy')
+    output_map = os.path.join(base_path, 'route_map.html')
+
+    try:
+        # Ejecutar el motor
+        sequence, distance = solve_logistics_route(
+            csv_path=csv_file, 
+            matrix_path=matrix_file, 
+            gap_rel=precision,
+            timeLimit=seconds
+        )
+
+        if sequence:
+            # Generar mapa
+            df = pd.read_csv(csv_file)
+            generate_route_map(df, sequence, output_file=output_map)
+            
+            return {
+                "message": "Optimization Complete",
+                "distance": round(distance, 4),
+                "config_used": {
+                    "gap_target": precision,
+                    "max_time_sec": seconds
+                },
+                "path": sequence
+            }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+if __name__ == "__main__":
+    # Configure tehe  server to run 'uvicorn backend.main:app --reload' from this python file
+    uvicorn.run(
+        "main:app",      # El archivo y la instancia de FastAPI
+        host="127.0.0.1", 
+        port=8000, 
+        reload=True      # Mantiene el auto-restart activo
+    )
+    # Run the interface from http://127.0.0.1:8000/docs or http://localhost:8000/docs
